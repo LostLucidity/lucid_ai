3061a6ee7fa58947edb707ba1ac046b5fc92e9df
unknown <lostlucidity@gmail.com>
2018-07-16 04:29:38 -0400

    Create nexus_command for worker building and chronoboosting.

--- lucid_bot.py
+++ lucid_bot.py
@@ -6,17 +6,33 @@ class LucidBot(sc2.BotAI):
   async def on_step(self, iteration):
     # gather resource
     await self.distribute_workers()
-    # build workers
-    await self.build_workers()
+    # build workers, chronoboosting
+    await self.nexus_command()
     # build supply
     await self.build_pylons()
     # scout
     # await self.scout()
 
-  async def build_workers(self):
-    for nexus in self.units(NEXUS).ready.noqueue:
-      if self.can_afford(PROBE):
-        await self.do(nexus.train(PROBE))
+  async def nexus_command(self):
+    for nexus in self.units(NEXUS).ready:
+      # build workers
+      if nexus.noqueue:
+        if self.can_afford(PROBE):
+          await self.do(nexus.train(PROBE))
+      # use chronoboost
+      abilities = await self.get_available_abilities(nexus)
+      if AbilityId.EFFECT_CHRONOBOOSTENERGYCOST in abilities:
+        if not nexus.has_buff(BuffId.CHRONOBOOSTENERGYCOST):
+            await self.do(nexus(AbilityId.EFFECT_CHRONOBOOSTENERGYCOST, nexus))
+
+  # async def build_workers(self):
+  #   for nexus in self.units(NEXUS).ready.noqueue:
+  #     if self.can_afford(PROBE):
+  #       await self.do(nexus.train(PROBE))
+
+  # async def chronoboosting(self):
+    
+
 
   async def build_pylons(self):
     if self.supply_left < self.supply_cap * 0.15:
