41a9a1d7e072d7697def69b08ad087d3d8228383
Daniel-PC\Daniel <lostlucidity@gmail.com>
2018-07-18 16:17:10 -0400

    Add attack threshhold to trigger attacks.

--- lucid_bot.py
+++ lucid_bot.py
@@ -3,8 +3,10 @@ import sc2
 from sc2.constants import *
 
 class LucidBot(sc2.BotAI):
-  attackThreshold = 1
+  
   async def on_step(self, iteration):
+    if iteration == 0:
+      self.attackThreshold = 1
     # gather resource
     await self.distribute_workers()
     # build workers, chronoboosting
@@ -13,6 +15,8 @@ class LucidBot(sc2.BotAI):
     await self.increase_supply()
     # build army
     await self.build_army()
+    # send army out.
+    await self.send_army()
     # scout
     # await self.scout()
 
@@ -70,3 +74,12 @@ class LucidBot(sc2.BotAI):
           if not self.already_pending(GATEWAY):
             pylons = self.units(PYLON)
             await self.build(GATEWAY, near=random.choice(pylons))
+
+  async def send_army(self):
+    # If army meets threshhold, attack.
+    zealots = self.units(ZEALOT)
+    if len(zealots) == self.attackThreshold:
+      # attack!
+      for zealot in zealots:
+        await self.do(zealot(AbilityId.PATROL, self.enemy_start_locations[0]))
+        # await self.do(zealot.patrol(self.enemy_start_locations[0]))
