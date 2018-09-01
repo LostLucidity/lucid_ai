b1377fb1df7213936d0f67099e4cd9ec77d738c8
Daniel-PC\Daniel <lostlucidity@gmail.com>
2018-09-01 11:46:58 -0400

    Add map list and randomly choose.

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import random
 import sc2, sys
 from __init__ import run_ladder_game
 from sc2 import Race, Difficulty
@@ -9,16 +10,18 @@ bot = Bot(Race.Protoss, LucidBot())
 
 # Start game
 if __name__ == '__main__':
-    if "--LadderServer" in sys.argv:
-        # Ladder game started by LadderManager
-        print("Starting ladder game...")
-        run_ladder_game(bot)
-    else:
-        # Local game
-        print("Starting local game...")
-        sc2.run_game(sc2.maps.get("Abyssal Reef LE"), [
-            bot,
-            # Computer(Race.Protoss, Difficulty.Medium)
-            Computer(Race.Protoss, Difficulty.Hard)
-            # Computer(Race.Protoss, Difficulty.VeryHard)
+	if "--LadderServer" in sys.argv:
+		# Ladder game started by LadderManager
+		print("Starting ladder game...")
+		run_ladder_game(bot)
+	else:
+		# Local game
+		print("Starting local game...")
+		# sc2.run_game(sc2.maps.get("Abyssal Reef LE"), [
+		map_name = random.choice(["Abyssal Reef LE", "BelShirVestigeLE", "CactusValleyLE", "HonorgroundsLE", "NewkirkPrecinctTE", "PaladinoTerminalLE", "ProximaStationLE"])
+		sc2.run_game(sc2.maps.get(map_name), [
+			bot,
+			# Computer(Race.Protoss, Difficulty.Medium)
+			# Computer(Race.Random, Difficulty.Hard)
+			Computer(Race.Terran, Difficulty.VeryHard)
 ], realtime=False)
