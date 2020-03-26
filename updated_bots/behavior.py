9b1ffde4fe5a165e9eb4e277da365dd282dd4203
Daniel-PC\Daniel <lostlucidity@gmail.com>
2020-03-26 13:37:06 -0400

    Create closest unit function for retreating.

--- updated_bots/behavior.py
+++ updated_bots/behavior.py
@@ -1,23 +1,31 @@
 from sc2.constants import AbilityId
 from sc2.ids.unit_typeid import UnitTypeId
 
+from helper import get_closest_unit
+
 def scan_surroundings(self):
   actions = []
   for unit in self.units:
     if self.enemy_units:
       closest_enemy = self.enemy_units.closest_to(unit)
-      if unit.position.distance_to(closest_enemy) <= closest_enemy.ground_range + 2:
-        actions += attack_or_regroup(self, unit, closest_enemy)
+      _range = closest_enemy.ground_range + 3
+      if unit.position.distance_to(closest_enemy) <= _range:
+        actions += attack_or_regroup(self, unit, closest_enemy, _range)
   return actions
 
-def attack_or_regroup(self, unit, enemy_unit):
+def attack_or_regroup(self, unit, enemy_unit, _range):
   # print('unit.army_size', unit.army_size)
   # print('enemy_unit.army_size', enemy_unit.army_size)
   if unit.army_size > enemy_unit.army_size:
     # check how orders look like
     return [ unit(AbilityId.ATTACK_ATTACK, enemy_unit)]
   else:
-    return [ unit(AbilityId.MOVE_MOVE, unit.position.towards(enemy_unit.position, -1)) ]
+    closest_ally = get_closest_unit(self, enemy_unit, self.units, _range)
+    closest_structure = get_closest_unit(self, enemy_unit, self.structures, _range)
+    if closest_ally:
+      return [ unit(AbilityId.MOVE_MOVE, closest_ally.position) ]
+    elif closest_structure:
+      return [ unit(AbilityId.MOVE_MOVE, closest_structure.position) ]
   return []
 
 def analyze_units(self):
--- updated_bots/helper.py
+++ updated_bots/helper.py
@@ -16,3 +16,35 @@ def short_on_workers(self):
 def calculate_building_amount(self, building, mineral_to_unit_build_rate):
   return math.floor(self.state.score.collection_rate_minerals / mineral_to_unit_build_rate)
 
+def get_closest_unit(self, unit, units, _range=0):
+  units_in_range = []
+  for _unit in units:
+    distance = _unit.distance_to(unit)
+    if distance > _range:
+      _unit.distance = distance
+      units_in_range.append(_unit)
+  if units_in_range:
+    return min(units_in_range, key=lambda _unit: _unit.distance)
+  # max_result = max(units_in_range, key=lambda _unit: _unit.distance)
+  # if self.iteration % 10 == 0:
+  #   print('units_in_range', units_in_range)
+  #   print('units_in_range[0]', units_in_range[0].distance)
+  #   print('units_in_range[len(units_in_range) - 1]', units_in_range[len(units_in_range) - 1].distance)
+  # print('min_result', min_result)
+  # print('min_result.distance', min_result.distance)
+  # print('max_result', max_result)
+  # print('max_result.distance', max_result.distance)
+  return []
+  # return min(((unit, dist) for unit, dist in zip(self, distances)), key=lambda my_tuple: my_tuple[1])[0]
+# def track_enemy_units(self):
+#   for unit in self.previous_enemy_units:
+#     found_index = next((index for (index, d) in enumerate(self.enemy_units) if d.tag == unit.tag), -1)
+#     if found_index < 0:
+#       self.missing_enemy_units.append(unit)
+
+# async def on_unit_destroyed(self, unit_tag):
+#   print('on_unit_destroyed', unit_tag)
+#   print('self.missing_enemy_units', self.missing_enemy_units)
+#   found_index = next((index for (index, d) in enumerate(self.missing_enemy_units) if d.tag == unit_tag), -1)
+#   if found_index >= 0:
+#     del self.missing_enemy_units[found_index]
