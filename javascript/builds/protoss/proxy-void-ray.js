34292ae91b034e91a0a5aecbb8addceb32049ab2
Daniel-PC\Daniel <lostlucidity@gmail.com>
2020-09-19 10:50:05 -0400

    Improve expansion selection for proxy, restrict probe construction, add attack logic.

--- javascript/builds/protoss/proxy-void-ray.js
+++ javascript/builds/protoss/proxy-void-ray.js
@@ -11,6 +11,11 @@ const buildWorkers = require("../../helper/build-workers");
 const { gridsInCircle } = require("@node-sc2/core/utils/geometry/angle");
 const { WARPGATERESEARCH } = require("@node-sc2/core/constants/upgrade");
 const { distance } = require("@node-sc2/core/utils/geometry/point");
+const { attack } = require("../../helper/army-behavior");
+
+let ATTACKFOOD = 34;
+let mainCombatTypes = [ VOIDRAY ];
+let supportUnitTypes = [ ADEPT, STALKER];
 
 const proxyVoidRay = createSystem({
   name: "Proxy Void Ray",
@@ -23,6 +28,49 @@ const proxyVoidRay = createSystem({
   },
   buildOrder: [
   ],
+  async onStep({ agent, data, resources }) {
+    const { foodUsed } = agent;
+    const { actions, units } = resources.get();
+    const collectedActions = [];
+    if (foodUsed >= 12) { await actions.sendAction(await tryBuilding(agent, data, resources, this.state, 0, placementConfigs.PYLON, [ await getFirstPylonPosition(resources)])); }
+    // add first pylon closer to nexus.
+    if (foodUsed >= 13) { await actions.sendAction(await tryBuilding(agent, data, resources, this.state, 0, placementConfigs.GATEWAY)); }
+    if (foodUsed == 14) { collectedActions.push(...await abilityOrder(data, resources, EFFECT_CHRONOBOOSTENERGYCOST, 1, NEXUS, NEXUS)); }
+    if (foodUsed >= 14) { collectedActions.push(...await tryBuilding(agent, data, resources, this.state, 0, placementConfigs.ASSIMILATOR)); }
+    if (foodUsed >= 16) { collectedActions.push(...await tryBuilding(agent, data, resources, this.state, 1, placementConfigs.ASSIMILATOR)); }
+    if (units.getById(ASSIMILATOR).length === 2) { collectedActions.push(...await moveToProxy(resources, this.state, 0)) }
+    if (foodUsed >= 18) { collectedActions.push(...await tryBuilding(agent, data, resources, this.state, 0, placementConfigs.CYBERNETICSCORE)); }
+    if (foodUsed >= 19) { collectedActions.push(...await tryBuilding(agent, data, resources, this.state, 1, placementConfigs.PYLON, [ this.state.proxyPosition ])); }
+    if (foodUsed >= 21) { await actions.sendAction(await tryBuilding(agent, data, resources, this.state, 0, placementConfigs.STARGATE, gridsInCircle(this.state.proxyPosition, 6.5))); }
+    if (foodUsed == 21) { collectedActions.push(...await trainOrder(data, units, ADEPT)); }
+    if (foodUsed == 23) { collectedActions.push(...await abilityOrder(data, resources, EFFECT_CHRONOBOOSTENERGYCOST, 1, NEXUS, GATEWAY)); }
+    if (foodUsed >= 23) { collectedActions.push(...await upgradeOrder(data, resources, WARPGATERESEARCH)); }
+    if (foodUsed >= 23) { collectedActions.push(...await tryBuilding(agent, data, resources, this.state, 2, placementConfigs.PYLON, [ await getThirdPylonPosition(resources, this.state) ])); }
+    if (foodUsed == 23) { collectedActions.push(...await trainOrder(data, units, STALKER)); }
+    if (foodUsed == 25 && !this.state.pauseBuilding) { await buildWorkers(agent, data, resources); }
+    if (foodUsed == 26) { collectedActions.push(...await trainOrder(data, units, VOIDRAY)); }
+    if (foodUsed == 30) { collectedActions.push(...await abilityOrder(data, resources, EFFECT_CHRONOBOOSTENERGYCOST, 1, NEXUS, STARGATE)); }
+    if (foodUsed >= 30) { collectedActions.push(...await tryBuilding(agent, data, resources, this.state, 0, placementConfigs.SHIELDBATTERY, gridsInCircle(this.state.thirdPylonPosition, 6.5))); }
+    if (foodUsed == 30) { collectedActions.push(...await trainOrder(data, units, VOIDRAY)); }
+    if (foodUsed == 34) { collectedActions.push(...await abilityOrder(data, resources, EFFECT_CHRONOBOOSTENERGYCOST, 1, NEXUS, STARGATE)); }
+    if (foodUsed >= 34) { collectedActions.push(...await tryBuilding(agent, data, resources, this.state, 1, placementConfigs.SHIELDBATTERY, gridsInCircle(this.state.thirdPylonPosition, 6.5))); }
+    if (foodUsed >= 34) { collectedActions.push(...await tryBuilding(agent, data, resources, this.state, 2, placementConfigs.SHIELDBATTERY, gridsInCircle(this.state.thirdPylonPosition, 6.5))); }
+    if (foodUsed == 34) { collectedActions.push(...await trainOrder(data, units, VOIDRAY)); }
+    if (foodUsed < ATTACKFOOD) { collectedActions.push(...attack(resources, mainCombatTypes, supportUnitTypes)); }
+    if (foodUsed >= ATTACKFOOD) { collectedActions.push(...attack(resources, mainCombatTypes, supportUnitTypes)); }
+    // state proxy position.
+    if (foodUsed < 21 && !this.state.pauseBuilding) { await buildWorkers(agent, data, resources); }
+    await actions.sendAction(collectedActions);
+  },
+  async onUnitDamaged({agent, data, resources}, damagedUnit) {
+    // near proxy position
+    const collectedActions = [];
+    if (distance(damagedUnit.pos, this.state.proxyPosition) < 6.5) {
+      collectedActions.push(...await tryBuilding(agent, data, resources, this.state, 2, placementConfigs.SHIELDBATTERY, gridsInCircle(damagedUnit.pos, 6.5)));
+      collectedActions.push(...await tryBuilding(agent, data, resources, this.state, 3, placementConfigs.SHIELDBATTERY, gridsInCircle(damagedUnit.pos, 6.5)));
+    }
+    return collectedActions
+  },
   async onUnitIdle({ resources }, idleUnit) {
     const {
       units
@@ -34,45 +82,22 @@ const proxyVoidRay = createSystem({
       }
     }
   },
-  async onStep({ agent, data, resources }) {
-    const { foodUsed } = agent;
-    const { actions, units } = resources.get();
-    const collectedActions = [];
-    if (foodUsed >= 12) { await actions.sendAction(await tryBuilding(agent, data, resources, this.state, 0, placementConfigs.PYLON, [ await getFirstPylonPosition(resources)])); }
-    // add first pylon closer to nexus.
-    if (foodUsed >= 13) { await actions.sendAction(await tryBuilding(agent, data, resources, this.state, 0, placementConfigs.GATEWAY)); }
-    if (foodUsed == 14) { collectedActions.push(...await abilityOrder(data, resources, EFFECT_CHRONOBOOSTENERGYCOST, 1, NEXUS, NEXUS)); }
-    if (foodUsed >= 14) { collectedActions.push(...await tryBuilding(agent, data, resources, this.state, 0, placementConfigs.ASSIMILATOR)); }
-    if (foodUsed >= 16) { collectedActions.push(...await tryBuilding(agent, data, resources, this.state, 1, placementConfigs.ASSIMILATOR)); }
-    if (units.getById(ASSIMILATOR).length === 2) { collectedActions.push(...await moveToProxy(resources, this.state, 0)) }  // proxy position based on closest non-natural(not third) to main
-    if (foodUsed >= 18) { collectedActions.push(...await tryBuilding(agent, data, resources, this.state, 0, placementConfigs.CYBERNETICSCORE)); }
-    if (foodUsed >= 19) { collectedActions.push(...await tryBuilding(agent, data, resources, this.state, 1, placementConfigs.PYLON, [ this.state.proxyPosition ])); }
-    if (foodUsed >= 21) { await actions.sendAction(await tryBuilding(agent, data, resources, this.state, 0, placementConfigs.STARGATE, gridsInCircle(this.state.proxyPosition, 6.5))); }
-    if (foodUsed == 21) { collectedActions.push(...await trainOrder(data, units, ADEPT)); }
-    if (foodUsed == 23) { collectedActions.push(...await abilityOrder(data, resources, EFFECT_CHRONOBOOSTENERGYCOST, 1, NEXUS, GATEWAY)); }
-    if (foodUsed >= 23) { collectedActions.push(...await upgradeOrder(data, resources, WARPGATERESEARCH)); }
-    if (foodUsed >= 23) { collectedActions.push(...await tryBuilding(agent, data, resources, this.state, 2, placementConfigs.PYLON, [ await getThirdPylonPosition(resources, this.state) ])); }
-    if (foodUsed == 23) { collectedActions.push(...await trainOrder(data, units, STALKER)); }
-    if (foodUsed == 26) { collectedActions.push(...await trainOrder(data, units, VOIDRAY)); }
-    if (foodUsed == 30) { collectedActions.push(...await abilityOrder(data, resources, EFFECT_CHRONOBOOSTENERGYCOST, 1, NEXUS, STARGATE)); }
-    if (foodUsed >= 30) { collectedActions.push(...await tryBuilding(agent, data, resources, this.state, 0, placementConfigs.SHIELDBATTERY, gridsInCircle(this.state.thirdPylonPosition, 6.5))); }
-    if (foodUsed == 30) { collectedActions.push(...await trainOrder(data, units, VOIDRAY)); }
-    if (foodUsed == 34) { collectedActions.push(...await abilityOrder(data, resources, EFFECT_CHRONOBOOSTENERGYCOST, 1, NEXUS, STARGATE)); }
-    if (foodUsed >= 34) { collectedActions.push(...await tryBuilding(agent, data, resources, this.state, 1, placementConfigs.SHIELDBATTERY, gridsInCircle(this.state.thirdPylonPosition, 6.5))); }
-    if (foodUsed >= 34) { collectedActions.push(...await tryBuilding(agent, data, resources, this.state, 2, placementConfigs.SHIELDBATTERY, gridsInCircle(this.state.thirdPylonPosition, 6.5))); }
-    // state proxy position.
-    if (foodUsed < 21 && !this.state.pauseBuilding) { await buildWorkers(agent, data, resources); }
-    if (foodUsed >= 26 && !this.state.pauseBuilding) { await buildWorkers(agent, data, resources); }
-    await actions.sendAction(collectedActions);
-  }
 })
+
+async function defendProxyPosition() {
+  // if marine starts attacking.
+}
 
 async function getProxyPosition(resources) {
   const { actions, map } = resources.get();
   const enemyMainPosition = map.getEnemyMain().townhallPosition;
-  const [ proxyExpansion ] = map.getEnemyThirds().sort((a, b) => distance(a.townhallPosition, enemyMainPosition) - distance(b.townhallPosition, enemyMainPosition));
+  const [ proxyExpansion ] = map.getExpansions()
+    .sort((a, b) => distance(a.townhallPosition, enemyMainPosition) - distance(b.townhallPosition, enemyMainPosition))
+    .filter(expansion => distance(expansion.townhallPosition, map.getEnemyMain().townhallPosition) > 1 && distance(expansion.townhallPosition, map.getEnemyNatural().townhallPosition) > 1);
   const placementGrids = proxyExpansion.areas.placementGrid;
-  return await actions.canPlace(PYLON, placementGrids);
+  const position = await actions.canPlace(PYLON, placementGrids);
+  console.log(distance(position, proxyExpansion.townhallPosition));
+  return position;
 };
 
 async function getFirstPylonPosition(resources) {
