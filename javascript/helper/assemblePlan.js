e94d55634477bff8ca302d0b853fae77854e04af
Daniel-PC\Daniel <lostlucidity@gmail.com>
2020-10-05 22:12:17 -0400

    Use alternative positions if natural wall fails to resolve.

--- javascript/helper/assemblePlan.js
+++ javascript/helper/assemblePlan.js
@@ -259,29 +259,33 @@ class AssemblePlan {
     }
     return placements;
   }
+
   findSupplyPositions() {
     const { map } = this.resources.get();
     const myExpansions = map.getOccupiedExpansions(Alliance.SELF);
     // front of natural pylon for great justice
     const naturalWall = map.getNatural().getWall();
-    let possiblePlacements = frontOfGrid(this.world, map.getNatural().areas.areaFill)
-        .filter(point => naturalWall.every(wallCell => (
-            (distance(wallCell, point) <= 6.5) &&
-            (distance(wallCell, point) >= 3)
-        )));
-  
-    if (possiblePlacements.length <= 0) {
+    let possiblePlacements = [];
+    if (naturalWall) {
+      possiblePlacements = frontOfGrid(this.world, map.getNatural().areas.areaFill)
+          .filter(point => naturalWall.every(wallCell => (
+              (distance(wallCell, point) <= 6.5) &&
+              (distance(wallCell, point) >= 3)
+          )));
+      if (possiblePlacements.length <= 0) {
         possiblePlacements = frontOfGrid(this.world, map.getNatural().areas.areaFill)
-            .map(point => {
-                point.coverage = naturalWall.filter(wallCell => (
-                    (distance(wallCell, point) <= 6.5) &&
-                    (distance(wallCell, point) >= 1)
-                )).length;
-                return point;
-            })
-            .sort((a, b) => b.coverage - a.coverage)
-            .filter((cell, i, arr) => cell.coverage === arr[0].coverage);
+          .map(point => {
+            point.coverage = naturalWall.filter(wallCell => (
+              (distance(wallCell, point) <= 6.5) &&
+              (distance(wallCell, point) >= 1)
+            )).length;
+            return point;
+          })
+          .sort((a, b) => b.coverage - a.coverage)
+          .filter((cell, i, arr) => cell.coverage === arr[0].coverage);
+      }
     }
+  
   
     return possiblePlacements;
   }
