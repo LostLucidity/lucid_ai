5a40c61773396bf2498e4d40f964f0fa725ee73b
Daniel-PC\Daniel <lostlucidity@gmail.com>
2020-10-07 07:39:24 -0400

    Add checks for tech, position and naturalwall.

--- javascript/builds/defense-setup.js
+++ javascript/builds/defense-setup.js
@@ -5,6 +5,7 @@ const { SHIELDBATTERY, PYLON, BUNKER, SPINECRAWLER } = require("@node-sc2/core/c
 const { gridsInCircle } = require("@node-sc2/core/utils/geometry/angle");
 const { avgPoints, distance } = require("@node-sc2/core/utils/geometry/point");
 const { Race } = require('@node-sc2/core/constants/enums');
+const canAfford = require("../helper/can-afford");
 
 module.exports = async function defenseSetup({ agent, data, resources }, state) {
   const {
@@ -20,19 +21,23 @@ module.exports = async function defenseSetup({ agent, data, resources }, state) 
         if ((units.getById(BUNKER).length + units.withCurrentOrders(buildAbilityId)) < 1 ) {
           const natural = map.getNatural();
           const naturalWall = natural.getWall();
-          const avg = avgPoints(naturalWall);
-          const avgWallAndNatural = avgPoints([avg, natural.townhallPosition]);
-          const nearPoints = gridsInCircle(avgWallAndNatural, 4);
-          const sampledPoints = nearPoints
-            .map(pos => ({ pos, rand: Math.random() }))
-            .sort((a, b) => a.rand - b.rand)
-            .map(a => a.pos)
-            .slice(0, 20);
-          const foundPosition = await actions.canPlace(BUNKER, sampledPoints);
-          try {
-            await actions.build(BUNKER, foundPosition);
-          } catch (error) {
-            console.log(error);
+          if (naturalWall) {
+            const avg = avgPoints(naturalWall);
+            const avgWallAndNatural = avgPoints([avg, natural.townhallPosition]);
+            const nearPoints = gridsInCircle(avgWallAndNatural, 4);
+            const sampledPoints = nearPoints
+              .map(pos => ({ pos, rand: Math.random() }))
+              .sort((a, b) => a.rand - b.rand)
+              .map(a => a.pos)
+              .slice(0, 20);
+            const foundPosition = await actions.canPlace(BUNKER, sampledPoints);
+            if (foundPosition) {
+              try {
+                await actions.build(BUNKER, foundPosition);
+              } catch (error) {
+                console.log(error);
+              }
+            }   
           }
         }
         break;
@@ -41,27 +46,31 @@ module.exports = async function defenseSetup({ agent, data, resources }, state) 
         if ((units.getById(SHIELDBATTERY).length + units.withCurrentOrders(buildAbilityId)) < 1 ) {
           const natural = map.getNatural();
           const naturalWall = natural.getWall();
-          const avg = avgPoints(naturalWall);
-          const [ closestPylon ] = units.getClosest(avg, units.getById(PYLON), 1);
-          const points = gridsInCircle(closestPylon.pos, 6.5);
-          const filteredPoints = points.filter(point => {
-            return (
-              distance(avg, point) > 3.25 &&
-              distance(natural.townhallPosition, point) > 3.75
-            );
-          });
-          // pick 10 random positions from the list
-          const randomPositions = filteredPoints
-            .map(pos => ({ pos, rand: Math.random() }))
-            .sort((a, b) => a.rand - b.rand)
-            .map(a => a.pos)
-            .slice(0, 20);
-          // see if any of them are good    
-          const foundPosition = await actions.canPlace(SHIELDBATTERY, randomPositions);
-          try {
-            await actions.build(SHIELDBATTERY, foundPosition);
-          } catch (error) {
-            console.log(error);
+          if (naturalWall) {
+            const avg = avgPoints(naturalWall);
+            const [ closestPylon ] = units.getClosest(avg, units.getById(PYLON), 1);
+            const points = gridsInCircle(closestPylon.pos, 6.5);
+            const filteredPoints = points.filter(point => {
+              return (
+                distance(avg, point) > 3.25 &&
+                distance(natural.townhallPosition, point) > 3.75
+              );
+            });
+            // pick 10 random positions from the list
+            const randomPositions = filteredPoints
+              .map(pos => ({ pos, rand: Math.random() }))
+              .sort((a, b) => a.rand - b.rand)
+              .map(a => a.pos)
+              .slice(0, 20);
+            // see if any of them are good    
+            const foundPosition = await actions.canPlace(SHIELDBATTERY, randomPositions);
+            if (foundPosition) {
+              try {
+                await actions.build(SHIELDBATTERY, foundPosition);
+              } catch (error) {
+                console.log(error);
+              }
+            }
           }
         }
         break;
@@ -70,25 +79,31 @@ module.exports = async function defenseSetup({ agent, data, resources }, state) 
         if ((units.getById(SPINECRAWLER).length + units.withCurrentOrders(buildAbilityId)) < 1 ) {
           const natural = map.getNatural();
           const naturalWall = natural.getWall();
-          const avg = avgPoints(naturalWall);
-          const points = gridsInCircle(avg, 1);
-          const filteredPoints = points.filter(point => {
-            return (
-              distance(natural.townhallPosition, point) > 3.75
-            );
-          });
-          // pick 10 random positions from the list
-          const randomPositions = filteredPoints
-            .map(pos => ({ pos, rand: Math.random() }))
-            .sort((a, b) => a.rand - b.rand)
-            .map(a => a.pos)
-            .slice(0, 20);
-          // see if any of them are good    
-          const foundPosition = await actions.canPlace(SPINECRAWLER, randomPositions);
-          try {
-            await actions.build(SPINECRAWLER, foundPosition);
-          } catch (error) {
-            console.log(error);
+          if (naturalWall) {
+            const avg = avgPoints(naturalWall);
+            const points = gridsInCircle(avg, 1);
+            const filteredPoints = points.filter(point => {
+              return (
+                distance(natural.townhallPosition, point) > 3.75
+              );
+            });
+            // pick 10 random positions from the list
+            const randomPositions = filteredPoints
+              .map(pos => ({ pos, rand: Math.random() }))
+              .sort((a, b) => a.rand - b.rand)
+              .map(a => a.pos)
+              .slice(0, 20);
+            // see if any of them are good    
+            const foundPosition = await actions.canPlace(SPINECRAWLER, randomPositions);
+            if (foundPosition) {
+              try {
+                if (canAfford(agent, data, SPINECRAWLER)) {
+                  await actions.build(SPINECRAWLER, foundPosition);
+                }
+              } catch (error) {
+                console.log(error);
+              }
+            }
           }
         }
         break;
