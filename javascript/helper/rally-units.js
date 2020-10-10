da1beb18e907bd26f365afebfb2ca6125f9a9217
Daniel-PC\Daniel <lostlucidity@gmail.com>
2020-10-10 12:39:54 -0400

    Load into bunkers.

--- javascript/helper/rally-units.js
+++ javascript/helper/rally-units.js
@@ -1,7 +1,8 @@
 //@ts-check
 "use strict"
 
-const { MOVE, ATTACK_ATTACK } = require("@node-sc2/core/constants/ability");
+const { MOVE, ATTACK_ATTACK, LOAD_BUNKER, SMART } = require("@node-sc2/core/constants/ability");
+const { BUNKER } = require("@node-sc2/core/constants/unit-type");
 const { tankBehavior } = require("./unit-behavior");
 
 function rallyUnits(resources, supportUnitTypes, rallyPoint=null) {
@@ -20,20 +21,49 @@ function rallyUnits(resources, supportUnitTypes, rallyPoint=null) {
     const supportUnits = [];
     supportUnitTypes.forEach(type => {
       supportUnits.concat(units.getById(type).filter(unit => !unit.labels.get('scout')));
-    });    
-    const unitCommand = {
-      abilityId: ATTACK_ATTACK,
-      targetWorldSpacePos: rallyPoint,
-      unitTags: combatUnits.map(unit => unit.tag),
-    }
-    collectedActions.push(unitCommand);
-    if (supportUnits.length > 0) {
+    });
+    if (units.getById(BUNKER).filter(bunker => bunker.buildProgress >= 1).length > 0) {
+      const [ bunker ] = units.getById(BUNKER);
+      rallyPoint = bunker.pos;
+      const closestCombatUnitTags = units.getClosest(bunker.pos, combatUnits, combatUnits.length, bunker.cargoSpaceMax - bunker.cargoSpaceTaken).map(unit => unit.tag);
+      if (bunker.abilityAvailable(LOAD_BUNKER)) {
+        const unitCommand = {
+          abilityId: SMART,
+          targetUnitTag: bunker.tag,
+          unitTags: closestCombatUnitTags,
+        }
+        collectedActions.push(unitCommand);
+      } else {
+        const unitCommand = {
+          abilityId: MOVE,
+          targetWorldSpacePos: rallyPoint,
+          unitTags: combatUnits.map(unit => unit.tag),
+        }
+        collectedActions.push(unitCommand);
+      }
+      if (supportUnits.length > 0) {
+        const unitCommand = {
+          abilityId: MOVE,
+          targetWorldSpacePos: rallyPoint,
+          unitTags: supportUnits.map(unit => unit.tag),
+        }
+        collectedActions.push(unitCommand);
+      }
+    } else {
       const unitCommand = {
-        abilityId: MOVE,
+        abilityId: ATTACK_ATTACK,
         targetWorldSpacePos: rallyPoint,
-        unitTags: supportUnits.map(unit => unit.tag),
+        unitTags: combatUnits.map(unit => unit.tag),
       }
       collectedActions.push(unitCommand);
+      if (supportUnits.length > 0) {
+        const unitCommand = {
+          abilityId: MOVE,
+          targetWorldSpacePos: rallyPoint,
+          unitTags: supportUnits.map(unit => unit.tag),
+        }
+        collectedActions.push(unitCommand);
+      }
     }
   }
   collectedActions.push(...tankBehavior(resources, rallyPoint));
