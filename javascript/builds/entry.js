7a597a0d3d9dd362ecaa6bbfe7035a3bd32377e5
Daniel-PC\Daniel <lostlucidity@gmail.com>
2020-10-18 23:49:12 -0400

    Cancel structure if under low health and under attack.

--- javascript/builds/entry.js
+++ javascript/builds/entry.js
@@ -2,6 +2,7 @@
 "use strict"
 
 const { createSystem } = require("@node-sc2/core");
+const { CANCEL_BUILDINPROGRESS } = require("@node-sc2/core/constants/ability");
 const { Alliance } = require("@node-sc2/core/constants/enums");
 const AssemblePlan = require("../helper/assemblePlan");
 const plans = require("./plans");
@@ -39,13 +40,20 @@ const entry = createSystem({
   async onUnitCreated(world, createdUnit) {
     await assemblePlan.onUnitCreated(world, createdUnit)
   },
-  async onUnitDamaged(world, damagedUnit) {
+  async onUnitDamaged({ resources }, damagedUnit) {
+    const { actions } = resources.get();
     // if unit damaged in in progrees and less than 1/3 health.
     // units.getStructures(structure => structure.health / structure.healthMax < 1 / 3);
     const totalHealthShield = damagedUnit.health + damagedUnit.shield;
     const maxHealthShield = damagedUnit.healthMax + damagedUnit.shieldMax;
     if ((totalHealthShield / maxHealthShield) < 1/3) {
-      const unitCommand = {};
+      if (damagedUnit.isStructure() && damagedUnit.buildProgress < 1) {
+        const unitCommand = {
+          abilityId: CANCEL_BUILDINPROGRESS,
+          unitTags: [ damagedUnit.tag ],
+        };
+        actions.sendAction(unitCommand);
+      }
     }
   },
   async onUnitIdle(world, idleUnit) {
