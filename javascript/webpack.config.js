815d016b5611828e1abcab5567238d67ecb026e5
Daniel-PC\Daniel <lostlucidity@gmail.com>
2020-10-19 23:28:24 -0400

    Add webpack config file.

---
+++ javascript/webpack.config.js
@@ -0,0 +1,15 @@
+//@ts-check
+"use strict"
+
+const path = require('path');
+
+module.exports = {
+  target: "node",
+  entry: {
+    app: ["./main.js"]
+  },
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "LucidZ.js"
+  }
+};
