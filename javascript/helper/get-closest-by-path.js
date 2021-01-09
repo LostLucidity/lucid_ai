7b5966f86fdd47e9068ab582557d566519441ae9
Daniel-PC\Daniel <lostlucidity@gmail.com>
2021-01-09 02:49:41 -0500

    Replace 0 path distance with 500 for proper sorted and comparison.

--- javascript/helper/get-closest-by-path.js
+++ javascript/helper/get-closest-by-path.js
@@ -7,18 +7,16 @@ module.exports = {
   distanceByPath: (resources, position, point) => {
     const { map, units } = resources.get();
     position = map.isPlaceable(position) ? position : getUnitCornerPosition(units.getClosest(position, units.getAlive())[0]);
-    return map.path(position, point).length
+    return map.path(position, point).length ? map.path(position, point).length : 500;
   },
   getClosestPositionByPath: (resources, position, points, n = 1) => {
     return points.map(point => ({ point, distance: module.exports.distanceByPath(resources, position, point) }))
-      .filter(pointObject => pointObject.distance > 0)
       .sort((a, b) => a.distance - b.distance)
       .map(pointObject => pointObject.point)
       .slice(0, n);
   },
   getClosestUnitByPath: (resources, position, units, n = 1) => {
     return units.map(unit => ({ unit, distance: module.exports.distanceByPath(resources, add(unit.pos, unit.radius), position) }))
-      .filter(pointObject => pointObject.distance > 0)
       .sort((a, b) => a.distance - b.distance)
       .map(u => u.unit)
       .slice(0, n);
