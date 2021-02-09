a9afab9483a87c08c4b81aba8da257860cd453cf
Daniel-PC\Daniel <lostlucidity@gmail.com>
2021-02-08 22:00:31 -0500

    Add scout location options that are specified in plans.

--- javascript/helper/location.js
+++ javascript/helper/location.js
@@ -2,7 +2,7 @@
 "use strict"
 
 const { Alliance } = require("@node-sc2/core/constants/enums");
-const { avgPoints } = require("@node-sc2/core/utils/geometry/point");
+const { avgPoints, add } = require("@node-sc2/core/utils/geometry/point");
 
 module.exports = {
   getCombatRally: (map, units) => {
@@ -20,5 +20,17 @@ module.exports = {
       x: Math.floor(Math.random() * Math.floor(map._mapSize.x)),
       y: Math.floor(Math.random() * Math.floor(map._mapSize.y)),
     };
+  },
+  acrossTheMap: (map) => {
+    const naturalToEnemyNaturalPath = map.path(add(map.getNatural().townhallPosition, 3), add(map.getEnemyNatural().townhallPosition, 3)).map(pathItem => ({ 'x': pathItem[0], 'y': pathItem[1] }));
+    const pathIncrements = Math.round(naturalToEnemyNaturalPath.length / 6);
+    const targetPosition = naturalToEnemyNaturalPath[naturalToEnemyNaturalPath.length - pathIncrements - 1];
+    return targetPosition;
+  },
+  outInFront: (map) => {
+    const naturalToEnemyNaturalPath = map.path(add(map.getNatural().townhallPosition, 3), add(map.getEnemyNatural().townhallPosition, 3)).map(pathItem => ({ 'x': pathItem[0], 'y': pathItem[1] }));
+    const pathIncrements = Math.round(naturalToEnemyNaturalPath.length / 6);
+    const targetPosition = naturalToEnemyNaturalPath[pathIncrements - 1];
+    return targetPosition;
   }
 }
