16f3b0da114b51eb5a89a6367df44a59db3e1fe3
Daniel-PC\Daniel <lostlucidity@gmail.com>
2021-02-25 22:51:40 -0500

    Remove unused imports.

--- javascript/builds/terran/mineral-field.js
+++ javascript/builds/terran/mineral-field.js
@@ -1,11 +1,6 @@
 //@ts-check
 "use strict"
 
-const { EFFECT_CALLDOWNMULE } = require("@node-sc2/core/constants/ability");
-const { ORBITALCOMMAND } = require("@node-sc2/core/constants/unit-type");
-const getRandom = require("@node-sc2/core/utils/get-random");
-const { getOccupiedExpansions, getBase } = require("../../helper/expansions");
-
 module.exports = {
   getMineralFieldTarget: (units, unit) => {
     const [ closestMineralField ] = units.getClosest(unit.pos, units.getMineralFields());
