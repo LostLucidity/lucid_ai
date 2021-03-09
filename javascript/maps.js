406fe7501fa568498dabac1e21420997a4b0e951
Daniel-PC\Daniel <lostlucidity@gmail.com>
2021-03-09 08:57:43 -0500

    Add 5.06 maps.

--- javascript/maps.js
+++ javascript/maps.js
@@ -10,6 +10,13 @@ maps = [
   "PillarsofGoldLE",
   "RomanticideLE",
   "SubmarineLE",
+  "DeathAura506",
+  "EternalEmpire506",
+  "EverDream506",
+  "GoldenWall506",
+  "IceandChrome506",
+  "PillarsofGold506",
+  "Submarine506",
 ]
 
 module.exports = maps;
