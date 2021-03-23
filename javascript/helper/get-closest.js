ffcc66cf0871fe653b4d68814b037f408fcef32f
Daniel-PC\Daniel <lostlucidity@gmail.com>
2021-03-22 22:33:06 -0400

    Create getClosest function for position to position.

---
+++ javascript/helper/get-closest.js
@@ -0,0 +1,13 @@
+//@ts-check
+"use strict"
+
+const { distance } = require("@node-sc2/core/utils/geometry/point");
+
+module.exports= {
+  getClosest: (position, locations, n = 1) => {
+    return locations.map(location => ({ location, distance: distance(position, location) }))
+      .sort((a, b) => a.distance - b.distance)
+      .map(u => u.location)
+      .slice(0, n);
+  }
+}
