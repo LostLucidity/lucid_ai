8d71814f6a0dcd6f4c7e2fda46e185926acd93a3
Daniel-PC\Daniel <lostlucidity@gmail.com>
2021-04-09 21:01:02 -0400

    Remove unused function.

--- javascript/helper/balance-resources.js
+++ javascript/helper/balance-resources.js
@@ -3,29 +3,6 @@
 
 const { GasMineRace } = require("@node-sc2/core/constants/race-map");
 
-async function balanceResources(agent, data, resources, ratio=2.4) {
-  const {
-    actions,
-    map,
-    units,
-  } = resources.get();
-  const { minerals, vespene } = agent;
-  const resourceRatio = minerals / vespene;
-  const gasUnitId = GasMineRace[agent.race]
-  const buildAbilityId = data.getUnitTypeData(gasUnitId).abilityId;
-  const [ geyser ] = map.freeGasGeysers();
-  const conditions = [
-    resourceRatio > ratio,
-    agent.canAfford(gasUnitId),
-    units.getById(gasUnitId).filter(unit => unit.buildProgress < 1).length < 1,
-    units.withCurrentOrders(buildAbilityId).length <= 0,
-    geyser,
-  ];
-  if (conditions.every(c => c)) {
-    try { await actions.buildGasMine(); } catch(error) { console.log(error.message); }
-  }
-}
-
 module.exports = {
   gasMineCheckAndBuild: async ({ agent, data, resources}, ratio=2.4) => {
     const {
