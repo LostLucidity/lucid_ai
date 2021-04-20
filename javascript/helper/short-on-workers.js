b8039f04f70e4709d9a61aee9a9716266d58b0dc
Daniel-PC\Daniel <lostlucidity@gmail.com>
2021-04-19 21:14:12 -0400

    Add surplus harvesters for townhall under construction.

--- javascript/helper/short-on-workers.js
+++ javascript/helper/short-on-workers.js
@@ -5,10 +5,11 @@ function shortOnWorkers(resources) {
   let idealHarvesters = 0
   let assignedHarvesters = 0
   const townhalls = units.getBases();
-    townhalls.forEach(townhall => {
-      idealHarvesters += townhall.idealHarvesters
-      assignedHarvesters += townhall.assignedHarvesters
-    });
+  townhalls.forEach(townhall => {
+    idealHarvesters += townhall.idealHarvesters;
+    assignedHarvesters += townhall.assignedHarvesters;
+    if (townhall.buildProgress < 1) { idealHarvesters += 3; }
+  });
   return idealHarvesters >= assignedHarvesters;
 }
 
