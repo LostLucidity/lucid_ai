2b706299daf200f977cbe16e6a95d2dc9b54a281
Daniel-PC\Daniel <lostlucidity@gmail.com>
2021-05-01 04:53:42 -0400

    Update function to transfer to minerals when needed.

--- javascript/systems/balance-resources.js
+++ javascript/systems/balance-resources.js
@@ -2,32 +2,57 @@
 "use strict"
 
 const { Alliance } = require("@node-sc2/core/constants/enums");
-const { gatheringAbilities } = require("@node-sc2/core/constants/groups");
+const { gatheringAbilities, mineralFieldTypes, gasMineTypes } = require("@node-sc2/core/constants/groups");
 const debugSilly = require('debug')('sc2:silly:WorkerBalance');
 
 module.exports = {
   balanceResources: async (resources, agent, targetRatio=16/6) => {
-    const { units } = resources.get();
+    const { actions, units } = resources.get();
     const readySelfFilter = { buildProgress: 1, alliance: Alliance.SELF };
-    const needyGasMine = units.getGasMines(readySelfFilter).find(u => u.assignedHarvesters < u.idealHarvesters);
+    const needyGasMines = units.getGasMines(readySelfFilter).find(u => u.assignedHarvesters < u.idealHarvesters);
     const { minerals, vespene } = agent;
     const resourceRatio = minerals / vespene;
     const surplusMinerals = resourceRatio > targetRatio;
-    if (needyGasMine && surplusMinerals) {
+    if (needyGasMines && surplusMinerals) {
       const townhalls = units.getAlive(readySelfFilter).filter(u => u.isTownhall());
       // const possibleDonerThs = townhalls.filter(townhall => townhall.assignedHarvesters > needyGasMine.assignedHarvesters + 1);
       // debugSilly('possible ths', possibleDonerThs.map(th => th.tag));
       // const [givingTownhall] = units.getClosest(needyGasMine.pos, possibleDonerThs);
       debugSilly('possible ths', townhalls.map(th => th.tag));
-      const [givingTownhall] = units.getClosest(needyGasMine.pos, townhalls);
-      const gatheringWorkers = units.getWorkers().filter(u => u.orders.some(o => [...gatheringAbilities].includes(o.abilityId)));
-      debugSilly('possible doners', gatheringWorkers.map(worker => worker.tag));
-      if (givingTownhall && gatheringWorkers.length > 0) {
+      const [givingTownhall] = units.getClosest(needyGasMines.pos, townhalls);
+      const gatheringMineralWorkers = units.getWorkers()
+        .filter(unit => unit.orders.some(order => {
+          return (
+            [...gatheringAbilities].includes(order.abilityId) &&
+            order.targetUnitTag &&
+            mineralFieldTypes.includes(units.getByTag(order.targetUnitTag).unitType)
+          );
+        }));
+      debugSilly('possible doners', gatheringMineralWorkers.map(worker => worker.tag));
+      if (givingTownhall && gatheringMineralWorkers.length > 0) {
         debugSilly('chosen closest th', givingTownhall.tag);
-        const [donatingWorker] = units.getClosest(givingTownhall.pos, gatheringWorkers);
+        const [donatingWorker] = units.getClosest(givingTownhall.pos, gatheringMineralWorkers);
         debugSilly('chosen worker', donatingWorker.tag);
-        const { actions } = resources.get();
-        await actions.mine([donatingWorker], needyGasMine, false);
+        await actions.mine([donatingWorker], needyGasMines, false);
+      }
+    }
+    const needyBases = units.getBases(readySelfFilter).find(u => u.assignedHarvesters < u.idealHarvesters + 2);
+    if (needyBases && !surplusMinerals) {
+      const gasMines = units.getAlive(readySelfFilter).filter(u => u.isGasMine());
+      const [givingGasMine] = units.getClosest(needyBases.pos, gasMines);
+      const gatheringGasWorkers = units.getWorkers()
+        .filter(unit => unit.orders.some(order => {
+          return (
+            [...gatheringAbilities].includes(order.abilityId) &&
+            order.targetUnitTag &&
+            gasMineTypes.includes(units.getByTag(order.targetUnitTag).unitType)
+          );
+        }));
+      if (givingGasMine && gatheringGasWorkers.length > 0) {
+        debugSilly('chosen closest th', givingGasMine.tag);
+        const [donatingWorker] = units.getClosest(givingGasMine.pos, gatheringGasWorkers);
+        debugSilly('chosen worker', donatingWorker.tag);
+        await actions.gather(donatingWorker, null, false);
       }
     }
   },
