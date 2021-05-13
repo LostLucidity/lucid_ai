973495a86433012a437696fecfbf55a54c22b41a
Daniel-PC\Daniel <lostlucidity@gmail.com>
2021-05-12 21:00:29 -0400

    Add space.

--- javascript/helper/get-closest.js
+++ javascript/helper/get-closest.js
@@ -3,7 +3,7 @@
 
 const { distance } = require("@node-sc2/core/utils/geometry/point");
 
-module.exports= {
+module.exports = {
   getClosest: (position, locations, n = 1) => {
     return locations.map(location => ({ location, distance: distance(position, location) }))
       .sort((a, b) => a.distance - b.distance)
