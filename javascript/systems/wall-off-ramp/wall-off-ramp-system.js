61855c52b753201a27a46eecb6a121ab8c03d93d
Daniel-PC\Daniel <lostlucidity@gmail.com>
2021-06-07 23:12:52 -0400

    Create wallOffRamp system to return wall of placement grids.

---
+++ javascript/systems/wall-off-ramp/wall-off-ramp-system.js
@@ -0,0 +1,25 @@
+//@ts-check
+"use strict"
+
+const { createSystem } = require("@node-sc2/core");
+const { distance } = require("@node-sc2/core/utils/geometry/point");
+const { getClosestPosition } = require("../../helper/get-closest");
+const wallOffRampService = require("./wall-off-ramp-service");
+const { distanceByPath } = require("../../helper/get-closest-by-path");
+
+module.exports = createSystem({
+  name: 'WallOffRamp',
+  type: 'agent',
+  async onGameStart({ resources }) {
+    const { debug, map } = resources.get();
+    debug.setDrawCells('ramps', map._ramps.map(r => ({ pos: r })), { size: 1, cube: true });
+    const naturalTownhallPosition = map.getNatural().townhallPosition;
+    wallOffRampService.adjacentToRampGrids = map.getMain().areas.placementGrid.filter(grid => {
+      return (
+        distance(grid, getClosestPosition(grid, map._ramps)[0]) < 2 &&
+        distanceByPath(resources, naturalTownhallPosition, grid) < distanceByPath(resources, naturalTownhallPosition, map.getMain().townhallPosition)
+      );
+    });
+    debug.setDrawCells('adToRamp', wallOffRampService.adjacentToRampGrids.map(r => ({ pos: r })), { size: 1, cube: true });
+  },
+});
