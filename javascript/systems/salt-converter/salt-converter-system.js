b9c880df3d7a3e2b21e7066ad915eee310f70d2b
Daniel-PC\Daniel <lostlucidity@gmail.com>
2021-06-10 22:10:54 -0400

    Create saltConverterSystem.

---
+++ javascript/systems/salt-converter/salt-converter-system.js
@@ -0,0 +1,27 @@
+//@ts-check
+"use strict"
+
+const { createSystem } = require("@node-sc2/core");
+const plans = require("../../builds/plans");
+const planService = require("../../services/plan-service");
+const { convertPlan } = require("./salt-converter");
+
+module.exports = createSystem({
+  name: 'SaltConverterSystem',
+  type: 'agent',
+  async onGameStart(world) {
+    const race = world.agent.race;
+    this.getBuild(race);
+  },
+  getBuild(race) {
+    const racePlans = plans[race];
+    const selectedPlan = racePlans['138658'];
+    if (selectedPlan.buildType === 'two variable') {
+      planService.scouts = selectedPlan.scouts;
+      planService.wallOff = selectedPlan.wallOff;
+      convertPlan(selectedPlan, race);
+    } else {
+      return selectedPlan;
+    }
+  },
+});
