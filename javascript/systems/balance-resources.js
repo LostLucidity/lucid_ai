44dee72943de64a3586a0da50c12d63017bd4545
Daniel-PC\Daniel <lostlucidity@gmail.com>
2021-06-12 12:56:35 -0400

    Add logic to build gas mines when needed.

--- javascript/systems/balance-resources.js
+++ javascript/systems/balance-resources.js
@@ -3,37 +3,42 @@
 
 const { Alliance } = require("@node-sc2/core/constants/enums");
 const { gatheringAbilities, mineralFieldTypes, gasMineTypes } = require("@node-sc2/core/constants/groups");
+const { gasMineCheckAndBuild } = require("../helper/balance-resources");
 const debugSilly = require('debug')('sc2:silly:WorkerBalance');
 
 module.exports = {
-  balanceResources: async (resources, targetRatio=16/6) => {
+  balanceResources: async ({ agent, data, resources }, targetRatio=16/6) => {
     const { actions, units } = resources.get();
     const readySelfFilter = { buildProgress: 1, alliance: Alliance.SELF };
     const needyGasMines = units.getGasMines(readySelfFilter).find(u => u.assignedHarvesters < u.idealHarvesters);
     const { mineralMinerCount, vespeneMinerCount } = getMinerCount(units);
     const mineralMinerCountRatio = mineralMinerCount / vespeneMinerCount;
     const surplusMinerals = mineralMinerCountRatio > targetRatio;
-    if (needyGasMines && surplusMinerals) {
-      const townhalls = units.getBases(readySelfFilter);
-      // const possibleDonerThs = townhalls.filter(townhall => townhall.assignedHarvesters > needyGasMine.assignedHarvesters + 1);
-      // debugSilly('possible ths', possibleDonerThs.map(th => th.tag));
-      // const [givingTownhall] = units.getClosest(needyGasMine.pos, possibleDonerThs);
-      debugSilly('possible ths', townhalls.map(th => th.tag));
-      const [givingTownhall] = units.getClosest(needyGasMines.pos, townhalls);
-      const gatheringMineralWorkers = units.getWorkers()
-        .filter(unit => unit.orders.some(order => {
-          return (
-            [...gatheringAbilities].includes(order.abilityId) &&
-            order.targetUnitTag &&
-            mineralFieldTypes.includes(units.getByTag(order.targetUnitTag).unitType)
-          );
-        }));
-      debugSilly('possible doners', gatheringMineralWorkers.map(worker => worker.tag));
-      if (givingTownhall && gatheringMineralWorkers.length > 0) {
-        debugSilly('chosen closest th', givingTownhall.tag);
-        const [donatingWorker] = units.getClosest(givingTownhall.pos, gatheringMineralWorkers);
-        debugSilly('chosen worker', donatingWorker.tag);
-        await actions.mine([donatingWorker], needyGasMines, false);
+    if (surplusMinerals) {
+      if (needyGasMines) {
+        const townhalls = units.getBases(readySelfFilter);
+        // const possibleDonerThs = townhalls.filter(townhall => townhall.assignedHarvesters > needyGasMine.assignedHarvesters + 1);
+        // debugSilly('possible ths', possibleDonerThs.map(th => th.tag));
+        // const [givingTownhall] = units.getClosest(needyGasMine.pos, possibleDonerThs);
+        debugSilly('possible ths', townhalls.map(th => th.tag));
+        const [givingTownhall] = units.getClosest(needyGasMines.pos, townhalls);
+        const gatheringMineralWorkers = units.getWorkers()
+          .filter(unit => unit.orders.some(order => {
+            return (
+              [...gatheringAbilities].includes(order.abilityId) &&
+              order.targetUnitTag &&
+              mineralFieldTypes.includes(units.getByTag(order.targetUnitTag).unitType)
+            );
+          }));
+        debugSilly('possible doners', gatheringMineralWorkers.map(worker => worker.tag));
+        if (givingTownhall && gatheringMineralWorkers.length > 0) {
+          debugSilly('chosen closest th', givingTownhall.tag);
+          const [donatingWorker] = units.getClosest(givingTownhall.pos, gatheringMineralWorkers);
+          debugSilly('chosen worker', donatingWorker.tag);
+          await actions.mine([donatingWorker], needyGasMines, false);
+        }
+      } else {
+        gasMineCheckAndBuild({ agent, data, resources })
       }
     }
     const needyBases = units.getBases(readySelfFilter).find(u => u.assignedHarvesters < u.idealHarvesters + 2);
