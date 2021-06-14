d348a77843999c732e8ae31bcfdd6da556fb2d82
Daniel-PC\Daniel <lostlucidity@gmail.com>
2021-06-13 20:02:04 -0400

    Fix order of operations.

--- javascript/helper.js
+++ javascript/helper.js
@@ -61,7 +61,7 @@ module.exports = {
       let abilityId = data.getUnitTypeData(type).abilityId;
       trainingUnitTypes.push(...units.withCurrentOrders(abilityId).map(() => type));
     });
-    return trainingUnitTypes.reduce((accumulator, unitType) => accumulator + unitType === ZERGLING ? 1 : data.getUnitTypeData(unitType).foodRequired, 0);
+    return trainingUnitTypes.reduce((accumulator, unitType) => accumulator + (unitType === ZERGLING ? 1 : data.getUnitTypeData(unitType).foodRequired), 0);
   },
   getSupply: (data, units) => {
     return units.reduce((accumulator, currentValue) => accumulator + data.getUnitTypeData(currentValue.unitType).foodRequired, 0);
