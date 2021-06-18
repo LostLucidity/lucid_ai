8515cde1d954994be18eb8e8691fbc43a7e22332
Daniel-PC\Daniel <lostlucidity@gmail.com>
2021-06-18 19:19:35 -0400

    Formatting.

--- javascript/helper.js
+++ javascript/helper.js
@@ -30,24 +30,24 @@ module.exports = {
     // front of natural pylon for great justice
     const naturalWall = map.getNatural().getWall();
     let possiblePlacements = frontOfGrid({ resources }, map.getNatural().areas.areaFill)
-        .filter(point => naturalWall.every(wallCell => (
-            (distance(wallCell, point) <= 6.5) &&
-            (distance(wallCell, point) >= 3)
-        )));
-  
+      .filter(point => naturalWall.every(wallCell => (
+        (distance(wallCell, point) <= 6.5) &&
+        (distance(wallCell, point) >= 3)
+      )));
+
     if (possiblePlacements.length <= 0) {
-        possiblePlacements = frontOfGrid({ resources }, map.getNatural().areas.areaFill)
-            .map(point => {
-                point.coverage = naturalWall.filter(wallCell => (
-                    (distance(wallCell, point) <= 6.5) &&
-                    (distance(wallCell, point) >= 1)
-                )).length;
-                return point;
-            })
-            .sort((a, b) => b.coverage - a.coverage)
-            .filter((cell, i, arr) => cell.coverage === arr[0].coverage);
+      possiblePlacements = frontOfGrid({ resources }, map.getNatural().areas.areaFill)
+        .map(point => {
+          point.coverage = naturalWall.filter(wallCell => (
+            (distance(wallCell, point) <= 6.5) &&
+            (distance(wallCell, point) >= 1)
+          )).length;
+          return point;
+        })
+        .sort((a, b) => b.coverage - a.coverage)
+        .filter((cell, i, arr) => cell.coverage === arr[0].coverage);
     }
-  
+
     return possiblePlacements;
   },
   getLoadedSupply: (units) => {
