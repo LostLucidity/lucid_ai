9f42b0e1d38f068711e9c64a62707db0e005e6fd
Daniel-PC\Daniel <lostlucidity@gmail.com>
2021-06-20 21:16:01 -0400

    Mostly finish ramp wall off.

--- javascript/systems/wall-off-ramp/wall-off-ramp-service.js
+++ javascript/systems/wall-off-ramp/wall-off-ramp-service.js
@@ -1,6 +1,7 @@
 //@ts-check
 "use strict"
 
+const { BARRACKS } = require("@node-sc2/core/constants/unit-type");
 const { gridsInCircle } = require("@node-sc2/core/utils/geometry/angle");
 const { distance } = require("@node-sc2/core/utils/geometry/point");
 const { getClosestPosition } = require("../../helper/get-closest");
@@ -11,13 +12,50 @@ const wallOffRampService = {
   findWallOffPlacement: (map, unitType) => {
     const placeableGrids = module.exports.adjacentToRampGrids.filter(grid => map.isPlaceable(grid));
     const cornerGrids = placeableGrids.filter(grid => intersectionOfPoints(gridsInCircle(grid, 1).filter(point => distance(point, grid) <= 1), placeableGrids).length === 2);
-    const [ closestCornerGrid ] = getClosestPosition(map.getMain().townhallPosition, cornerGrids);
+    const [closestCornerGrid] = getClosestPosition(map.getMain().townhallPosition, cornerGrids);
     let wallOffPosition = [];
     if (closestCornerGrid) {
-      wallOffPosition.push(...getClosestPosition(closestCornerGrid, gridsInCircle(closestCornerGrid, 3).filter(grid => map.isPlaceableAt(unitType, grid))));
+      const cornerGridCircle = gridsInCircle(closestCornerGrid, 3);
+      let closestPlaceableGrids = getClosestPosition(closestCornerGrid, cornerGridCircle, cornerGridCircle.length).filter(grid => {
+        if (unitType === BARRACKS) {
+          return isBuildingAndAddonPlaceable(map, unitType, grid);
+        } else {
+          return map.isPlaceableAt(unitType, grid);
+        }
+      });
+      const [closestRamp] = getClosestPosition(closestCornerGrid, cornerGridCircle.filter(grid => map.isRamp(grid)));
+      if (closestRamp) {
+        if (unitType === BARRACKS) {
+          closestPlaceableGrids = closestPlaceableGrids.map(grid => {
+            if (distance(grid, closestRamp) < distance(getAddOnPlacement(grid), closestRamp)) {
+              return grid;
+            } else {
+              return getAddOnPlacement(grid);
+            }
+          });
+        }
+        const [closestPlaceableToRamp] = getClosestPosition(closestRamp, closestPlaceableGrids)
+        if (closestPlaceableToRamp) {
+          let position = null;
+          if (unitType !== BARRACKS || isBuildingAndAddonPlaceable(map, unitType, closestPlaceableToRamp)) {
+            position = closestPlaceableToRamp;
+          } else {
+            position = getAddOnBuildingPlacement(closestPlaceableToRamp);
+          }
+          wallOffPosition.push(position);
+        }
+      }
     }
     return wallOffPosition;
   }
+}
+
+function getAddOnBuildingPlacement(position) {
+  return { x: position.x - 3, y: position.y }
+}
+
+function getAddOnPlacement(position) {
+  return { x: position.x + 3, y: position.y - 0 }
 }
 
 module.exports = wallOffRampService;
