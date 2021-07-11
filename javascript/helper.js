e570a3a039302af7b1489663943641068a2019ea
Daniel-PC\Daniel <lostlucidity@gmail.com>
2021-07-10 22:59:16 -0400

    Move checkUnitCount to track-units-service.

--- javascript/helper.js
+++ javascript/helper.js
@@ -9,7 +9,6 @@ const { PROBE, ZERGLING } = require('@node-sc2/core/constants/unit-type');
 const { distance } = require('@node-sc2/core/utils/geometry/point');
 const { frontOfGrid } = require('@node-sc2/core/utils/map/region');
 const { countTypes } = require('./helper/groups');
-const trackUnitsService = require('./systems/track-units/track-units-service');
 
 module.exports = {
   checkBuildingCount: ({ agent, data, resources }, unitType, targetCount) => {
@@ -25,16 +24,6 @@ module.exports = {
       count += unitsToCount.length;
     });
     return count === targetCount;
-  },
-  checkUnitCount: ({ data, resources }, unitType, targetCount) => {
-    const { units } = resources.get();
-    const orders = [];
-    let abilityId = data.getUnitTypeData(unitType).abilityId;
-    units.withCurrentOrders(abilityId).forEach(unit => {
-      unit.orders.forEach(order => { if (order.abilityId === abilityId) { orders.push(order); } });
-    });
-    const unitCount = units.getById(unitType).length + orders.length + trackUnitsService.missingUnits.filter(unit => unit.unitType === unitType).length;
-    return unitCount === targetCount;
   },
   findSupplyPositions: (resources) => {
     const { map } = resources.get();
--- javascript/systems/track-units/track-units-service.js
+++ javascript/systems/track-units/track-units-service.js
@@ -2,11 +2,28 @@
 "use strict"
 
 const { getSupply, getTrainingSupply } = require("../../helper");
+const { morphMapping } = require("../../helper/groups");
 const planService = require("../../services/plan-service");
 
 const trackUnitsService = {
   previousUnits: [],
   missingUnits: [],
+  checkUnitCount: ({ data, resources }, unitType, targetCount) => {
+    const { units } = resources.get();
+    const orders = [];
+    let unitTypes = [];
+    if (morphMapping.has(unitType)) {
+      unitTypes = morphMapping.get(unitType);
+    } else {
+      unitTypes = [unitType];
+    }
+    let abilityId = data.getUnitTypeData(unitType).abilityId;
+    units.withCurrentOrders(abilityId).forEach(unit => {
+      unit.orders.forEach(order => { if (order.abilityId === abilityId) { orders.push(order); } });
+    });
+    const unitCount = units.getById(unitTypes).length + orders.length + trackUnitsService.missingUnits.filter(unit => unit.unitType === unitType).length;
+    return unitCount === targetCount;
+  },
   getSelfCombatSupply: (world) => {
     const { data, resources } = world;
     return getSupply(data, resources.get().units.getCombatUnits()) + getTrainingSupply(world, planService.trainingTypes) + trackUnitsService.missingUnits.reduce((foodCount, missingUnit) => {
