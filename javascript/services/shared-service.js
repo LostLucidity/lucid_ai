db2378be5cf76c955ce92c3bfd06bec5dc6d1f88
Daniel-PC\Daniel <lostlucidity@gmail.com>
2021-07-11 10:56:51 -0400

    Add additional check for array length for pendingOrders.

--- javascript/services/shared-service.js
+++ javascript/services/shared-service.js
@@ -6,14 +6,14 @@ const { Alliance } = require("@node-sc2/core/constants/enums");
 const sharedService = {
   removePendingOrderBySystemName: (units, name) => {
     units.getAlive(Alliance.SELF).forEach(unit => {
-      if (unit['pendingOrders']) {
-        unit['pendingOrders'] = unit['pendingOrders'].filter(order => order.system === name);
+      if (unit['pendingOrders'] && unit['pendingOrders'].length > 0) {
+        unit['pendingOrders'] = unit['pendingOrders'].filter(order => order.system !== name);
       }
     });
   },
   setPendingOrderBySystemName: (units, name) => {
     units.getAlive().forEach(unit => {
-      if (unit['pendingOrders']) {
+      if (unit['pendingOrders'] && unit['pendingOrders'].length > 0) {
         unit['pendingOrders'].forEach(order => order.system = name);
       }
     });
