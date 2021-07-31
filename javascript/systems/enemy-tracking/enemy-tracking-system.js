0882332733a5c762606e183a0a396726ecca7c2d
Daniel-PC\Daniel <lostlucidity@gmail.com>
2021-07-30 20:23:58 -0400

    Set supply power for each unit in enemyTrackingSystem.

--- javascript/systems/enemy-tracking/enemy-tracking-system.js
+++ javascript/systems/enemy-tracking/enemy-tracking-system.js
@@ -1,13 +1,18 @@
 //@ts-check
 "use strict"
 
-const { createSystem } = require("@node-sc2/core")
+const { createSystem } = require("@node-sc2/core");
+const { setSelfSupplyPowers, setEnemySupplyPowers } = require("../../services/shared-service");
+const trackUnitsService = require("../track-units/track-units-service");
 const enemyTrackingService = require("./enemy-tracking-service")
 
 module.exports = createSystem({
   name: 'EnemyTrackingSystem',
   type: 'agent',
-  async onStep({ resources }) {
+  async onStep({ data, resources }) {
     enemyTrackingService.setBaseThreats(resources);
+    const { enemyUnits } = enemyTrackingService;
+    setSelfSupplyPowers(data, enemyUnits);
+    setEnemySupplyPowers(data, enemyUnits, trackUnitsService.selfUnits);
   }
 });
