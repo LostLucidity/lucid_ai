9ab2cebadef3411c8b1c046e2feb948cdba5a90d
Daniel-PC\Daniel <lostlucidity@gmail.com>
2021-08-10 21:39:08 -0400

    Add food for step logging.

--- javascript/systems/logging-system.js
+++ javascript/systems/logging-system.js
@@ -6,7 +6,7 @@ const { createSystem } = require("@node-sc2/core");
 module.exports = createSystem({
   name: 'Logging',
   type: 'agent',
-  async onStep({ resources }) {
-    console.log(resources.get().frame.timeInSeconds());
+  async onStep({ agent, resources }) {
+    console.log(agent.foodUsed, resources.get().frame.timeInSeconds());
   },
 });
