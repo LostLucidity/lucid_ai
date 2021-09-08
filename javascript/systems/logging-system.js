4855cb5572bb947c891179bc1f9332878552d0d7
Daniel-PC\Daniel <lostlucidity@gmail.com>
2021-09-08 08:27:33 -0400

    Log pauseBuilding status onStep.

--- javascript/systems/logging-system.js
+++ javascript/systems/logging-system.js
@@ -2,11 +2,12 @@
 "use strict"
 
 const { createSystem } = require("@node-sc2/core");
+const planService = require("../services/plan-service");
 
 module.exports = createSystem({
   name: 'Logging',
   type: 'agent',
   async onStep({ agent, resources }) {
-    console.log(agent.foodUsed, resources.get().frame.timeInSeconds());
+    console.log(agent.foodUsed, resources.get().frame.timeInSeconds(), planService.pauseBuilding);
   },
 });
