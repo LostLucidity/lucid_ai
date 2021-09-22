4108756a55fad7be1a67e8dfd66b2889e26731ea
Daniel-PC\Daniel <lostlucidity@gmail.com>
2021-09-22 07:46:30 -0400

    Fix frontOfGrid argument.

--- javascript/helper/placement/placement-helper.js
+++ javascript/helper/placement/placement-helper.js
@@ -130,14 +130,14 @@ const placementHelper = {
     const { map } = resources.get();
     // front of natural pylon for great justice
     const naturalWall = map.getNatural().getWall();
-    let possiblePlacements = frontOfGrid(resources, map.getNatural().areas.areaFill)
+    let possiblePlacements = frontOfGrid({resources}, map.getNatural().areas.areaFill)
         .filter(point => naturalWall.every(wallCell => (
             (distance(wallCell, point) <= 6.5) &&
             (distance(wallCell, point) >= 3)
         )));
   
     if (possiblePlacements.length <= 0) {
-        possiblePlacements = frontOfGrid(resources, map.getNatural().areas.areaFill)
+        possiblePlacements = frontOfGrid({resources}, map.getNatural().areas.areaFill)
             .map(point => {
                 point.coverage = naturalWall.filter(wallCell => (
                     (distance(wallCell, point) <= 6.5) &&
