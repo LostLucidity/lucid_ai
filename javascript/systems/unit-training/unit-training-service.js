4741822a226d3b81232b3ca077973c189a78aac6
Daniel-PC\Daniel <lostlucidity@gmail.com>
2021-10-10 20:52:55 -0400

    Add additional training check for techLab requirements.

--- javascript/systems/unit-training/unit-training-service.js
+++ javascript/systems/unit-training/unit-training-service.js
@@ -2,18 +2,43 @@
 "use strict"
 
 const { WarpUnitAbility } = require("@node-sc2/core/constants");
-const { WARPGATE } = require("@node-sc2/core/constants/unit-type");
+const { WARPGATE, TECHLAB } = require("@node-sc2/core/constants/unit-type");
 
 const unitTrainingService = {
   selectedTypeToBuild: null,
+  /**
+   * Check unit can train now.
+   * @param {World["data"]} data
+   * @param {Unit} unit 
+   * @param {UnitTypeId} unitType
+   * @returns {boolean}
+   */
+  canTrainNow: (data, unit, unitType) => {
+    const conditions = [unit.noQueue || (unit.hasReactor() && unit.orders.length < 2)];
+    const { techRequirement } = data.getUnitTypeData(unitType);
+    if (techRequirement && techRequirement === TECHLAB) {
+      conditions.push(unit.hasTechLab());
+    }
+    return conditions.every(condition => condition);
+  },
+  /**
+   * Check if unitType has prerequisites to build when minerals are available.
+   * @param {World} world 
+   * @param {UnitTypeId} unitType 
+   * @returns {boolean}
+   */
   haveAvailableProductionUnitsFor: (world, unitType) => {
     const { data, resources } = world;
     const { units } = resources.get();
     const warpInAbilityId = WarpUnitAbility[unitType];
-    const { abilityId } = data.getUnitTypeData(unitType);
     return (
       units.getById(WARPGATE).some(warpgate => warpgate.abilityAvailable(warpInAbilityId)) ||
-      units.getProductionUnits(unitType).some(unit => (unit.noQueue || (unit.hasReactor() && unit.orders.length < 2)))
+      units.getProductionUnits(unitType).some(unit => {
+        return (
+          unitTrainingService.canTrainNow(data, unit, unitType) &&
+          unit.buildProgress >= 1
+        )
+      })
     );
   },
   workersTrainingTendedTo: false,
