475da8f7f487a1e5ea9d48e493d69f02023a6cc4
Daniel-PC\Daniel <lostlucidity@gmail.com>
2021-10-28 18:12:32 -0400

    Keep harassers together.

--- javascript/builds/harass.js
+++ javascript/builds/harass.js
@@ -6,8 +6,16 @@ const { avgPoints, distance } = require("@node-sc2/core/utils/geometry/point");
 const { Alliance } = require('@node-sc2/core/constants/enums');
 const microService = require("../services/micro-service");
 const { microRangedUnit } = require("../services/micro-service");
+const { createUnitCommand } = require("../services/actions-service");
+const { ATTACK_ATTACK, MOVE } = require("@node-sc2/core/constants/ability");
 
 module.exports = {
+  /**
+   * 
+   * @param {World} world 
+   * @param {{ harassOn: boolean; }} state 
+   * @returns {Promise<void>}
+   */
   harass: async (world, state) => {
     const { data, resources } = world;
     const { actions, map, units } = resources.get();
@@ -22,31 +30,36 @@ module.exports = {
       const harassers = units.withLabel(label);
       const positionsOfHarassers = harassers.map(harasser => harasser.pos);
       const averagePoints = avgPoints(positionsOfHarassers);
+      const enemyWorkers = [];
       const enemyUnits = units.getAlive(Alliance.ENEMY).filter(unit => {
-        return (
-          !(unit.unitType === EGG) &&
-          !(unit.unitType === LARVA) &&
-          !(unit.unitType === HATCHERY) &&
-          !(unit.unitType === COMMANDCENTER) &&
-          !(unit.unitType === ORBITALCOMMAND) &&
-          !(unit.unitType === NEXUS)
-        )
+        if (unit.isWorker()) enemyWorkers.push(unit);
+        return true;
       });
-      let [closestEnemyUnit] = units.getClosest(averagePoints, enemyUnits, 1);
+      let [closestEnemyUnit] = units.getClosest(averagePoints, enemyWorkers);
+      closestEnemyUnit = closestEnemyUnit ? closestEnemyUnit : units.getClosest(averagePoints, enemyUnits)[0];
       if (units.withLabel(label).filter(harasser => harasser.labels.get(label)).length === 4) {
         if (closestEnemyUnit && distance(closestEnemyUnit.pos, averagePoints) <= 8) {
           const harasserActions = [];
           harassers.forEach(harasser => harasserActions.push(...microRangedUnit(data, harasser, closestEnemyUnit)));
-          return actions.sendAction(harasserActions);
+          await actions.sendAction(harasserActions);
         } else {
-          return actions.attackMove(harassers, map.getEnemyNatural().townhallPosition);
+          const outOfGroupHarassers = [];
+          const groupedHarassers = harassers.filter(harasser => {
+            if (distance(harasser.pos, averagePoints) > 8) outOfGroupHarassers.push(harasser);
+            else return true;
+          });
+          const groupedHarassersCommand = createUnitCommand(ATTACK_ATTACK, groupedHarassers);
+          groupedHarassersCommand.targetWorldSpacePos = map.getEnemyNatural().townhallPosition;
+          const outOfGroupHarasserCommand = createUnitCommand(MOVE, outOfGroupHarassers);
+          outOfGroupHarasserCommand.targetWorldSpacePos = averagePoints;
+          await actions.sendAction([groupedHarassersCommand, outOfGroupHarasserCommand]);
         }
       } else {
         if (!closestEnemyUnit || distance(closestEnemyUnit.pos, averagePoints) > 8) {
           state.harassOn = false;
           harassers.forEach(harasser => harasser.labels.delete(label));
         }
-        return actions.move(harassers, map.getCombatRally());
+        await actions.move(harassers, map.getCombatRally());
       }
     }
   }
