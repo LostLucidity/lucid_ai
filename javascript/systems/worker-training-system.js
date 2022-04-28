5f78abdaf935b8738c3c52c3043bbcab92879792
Daniel-PC\Daniel <lostlucidity@gmail.com>
2022-04-28 07:19:26 -0400

    Run trainWorkers on gameStart.

--- javascript/systems/worker-training-system.js
+++ javascript/systems/worker-training-system.js
@@ -13,32 +13,42 @@ const unitTrainingService = require("./unit-training/unit-training-service");
 module.exports = createSystem({
   name: 'WorkerTrainingSystem',
   type: 'agent',
+  async onGameStart(world) {
+    await trainWorkers(world);
+  },
   async onStep(world) {
-    const { agent, resources } = world;
-    const { race } = agent;
-    const { units } = resources.get();
-    const workerCount = units.getById(WorkerRace[race]).length;
-    const assignedWorkerCount = [...units.getBases(), ...units.getById(GasMineRace[race])].reduce((assignedWorkerCount, base) => base.assignedHarvesters + assignedWorkerCount, 0);
-    const minimumWorkerCount = Math.min(workerCount, assignedWorkerCount);
-    const { outpowered, unitProductionAvailable } = worldService
-    let conditionsMet = planService.bogIsActive && minimumWorkerCount <= 11;
-    if (!planService.bogIsActive) {
-      const conditions = [
-        haveAvailableProductionUnitsFor(world, WorkerRace[agent.race]),
-        !planService.isPlanPaused,
-        agent.minerals < 512 || minimumWorkerCount <= 36,
-        shortOnWorkers(resources),
-        !outpowered || (outpowered && !unitProductionAvailable)
-      ];
-      conditionsMet = conditions.every(condition => condition);
-    }
-    if (conditionsMet) {
-      unitTrainingService.workersTrainingTendedTo = false;
-      const { abilityId } = world.data.getUnitTypeData(WorkerRace[race])
-      const productionUnit = resources.get().units.getProductionUnits(WorkerRace[race]).find(u => u.noQueue && u.abilityAvailable(abilityId));
-      try { if (productionUnit) await buildWorkers(world); } catch (error) { console.log(error); }
-    } else {
-      unitTrainingService.workersTrainingTendedTo = true;
-    }
+    await trainWorkers(world);
   }
 });
+/**
+ * @param {World} world 
+ * @returns {Promise<void>}
+ */
+async function trainWorkers(world) {
+  const { agent, resources } = world;
+  const { race } = agent;
+  const { units } = resources.get();
+  const workerCount = units.getById(WorkerRace[race]).length;
+  const assignedWorkerCount = [...units.getBases(), ...units.getById(GasMineRace[race])].reduce((assignedWorkerCount, base) => base.assignedHarvesters + assignedWorkerCount, 0);
+  const minimumWorkerCount = Math.min(workerCount, assignedWorkerCount);
+  const { outpowered, unitProductionAvailable } = worldService
+  let conditionsMet = planService.bogIsActive && minimumWorkerCount <= 11;
+  if (!planService.bogIsActive) {
+    const conditions = [
+      haveAvailableProductionUnitsFor(world, WorkerRace[agent.race]),
+      !planService.isPlanPaused,
+      agent.minerals < 512 || minimumWorkerCount <= 36,
+      shortOnWorkers(resources),
+      !outpowered || (outpowered && !unitProductionAvailable)
+    ];
+    conditionsMet = conditions.every(condition => condition);
+  }
+  if (conditionsMet) {
+    unitTrainingService.workersTrainingTendedTo = false;
+    const { abilityId } = world.data.getUnitTypeData(WorkerRace[race])
+    const productionUnit = resources.get().units.getProductionUnits(WorkerRace[race]).find(u => u.noQueue && u.abilityAvailable(abilityId));
+    try { if (productionUnit) await buildWorkers(world); } catch (error) { console.log(error); }
+  } else {
+    unitTrainingService.workersTrainingTendedTo = true;
+  }
+}
