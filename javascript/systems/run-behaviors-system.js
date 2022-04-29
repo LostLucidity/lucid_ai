0719babf7c69f7ebcf9b27136dc56362bd44aa7c
Daniel-PC\Daniel <lostlucidity@gmail.com>
2022-04-28 20:08:49 -0400

    Add behaviors to run-behaviors-system.

--- javascript/systems/run-behaviors-system.js
+++ javascript/systems/run-behaviors-system.js
@@ -2,8 +2,9 @@
 "use strict"
 
 const { createSystem } = require("@node-sc2/core");
+const { generalScouting } = require("../builds/scouting");
 const { scoutEnemyMainBehavior, clearFromEnemyBehavior } = require("../helper/behavior/labelled-behavior");
-const { supplyDepotBehavior } = require("../helper/behavior/unit-behavior");
+const { supplyDepotBehavior, overlordBehavior } = require("../helper/behavior/unit-behavior");
 const scoutService = require("./scouting/scouting-service");
 
 module.exports = createSystem({
@@ -16,11 +17,15 @@ module.exports = createSystem({
     scoutService.opponentRace = seenEnemyUnit.data().race;
   },
   async onStep(world) {
-    const { data, resources } = world;
+    const { resources } = world;
+    const { actions } = resources.get();
     const collectedActions = [];
     collectedActions.push(...clearFromEnemyBehavior(world));
-    await scoutEnemyMainBehavior({ data, resources }, scoutService.opponentRace);
+    await scoutEnemyMainBehavior(world);
     collectedActions.push(...supplyDepotBehavior(resources));
-    await resources.get().actions.sendAction(collectedActions);
+    await actions.sendAction(collectedActions);
+  },
+  async onUnitCreated(world, createdUnit) {
+    await generalScouting(world, createdUnit);
   },
 });
