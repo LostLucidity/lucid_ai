418b78901b288cde6fe262cf3a7c2c02804e2b1c
Daniel-PC\Daniel <lostlucidity@gmail.com>
2022-05-19 23:05:01 -0400

    Remove unused behavior.

--- javascript/systems/run-behaviors-system.js
+++ javascript/systems/run-behaviors-system.js
@@ -4,7 +4,7 @@
 const { createSystem } = require("@node-sc2/core");
 const { generalScouting } = require("../builds/scouting");
 const { scoutEnemyMainBehavior, clearFromEnemyBehavior } = require("../helper/behavior/labelled-behavior");
-const { supplyDepotBehavior, overlordBehavior } = require("../helper/behavior/unit-behavior");
+const { supplyDepotBehavior } = require("../helper/behavior/unit-behavior");
 const scoutService = require("./scouting/scouting-service");
 
 module.exports = createSystem({
