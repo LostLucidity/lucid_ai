40b99f236f8f4a316fc9dce21027ceba8a334e8b
Daniel-PC\Daniel <lostlucidity@gmail.com>
2022-10-02 10:07:09 -0400

    Move overlord to proper townhall position.

--- javascript/builds/scouting.js
+++ javascript/builds/scouting.js
@@ -3,7 +3,9 @@
 
 const { MOVE } = require("@node-sc2/core/constants/ability");
 const { Alliance } = require("@node-sc2/core/constants/enums");
+const { TownhallRace } = require("@node-sc2/core/constants/race-map");
 const { OVERLORD } = require("@node-sc2/core/constants/unit-type");
+const { getMiddleOfStructure } = require("../services/position-service");
 
 module.exports = {
   cancelEarlyScout: (units) => {
@@ -17,23 +19,30 @@ module.exports = {
       });
     }
   },
+  /**
+   * @param {World} world 
+   * @param {Unit} unit 
+   */
   generalScouting: async (world, unit) => {
-    const { actions, map, units } = world.resources.get();
+    const { agent, resources } = world;
+    const { actions, map, units } = resources.get();
     const collectedActions = [];
     if (unit.unitType === OVERLORD) {
       const unitCommand = {
         abilityId: MOVE,
         unitTags: [ unit.tag ],
       };
+      const townhall = TownhallRace[agent.race][0];
       if (units.getById(OVERLORD).length === 1) {
-        unitCommand.targetWorldSpacePos = map.getEnemyNatural().townhallPosition;
+        getMiddleOfStructure(map.getEnemyNatural().townhallPosition, TownhallRace[agent.race][0]);
+        unitCommand.targetWorldSpacePos = getMiddleOfStructure(map.getEnemyNatural().townhallPosition, townhall);
       } else if (units.getById(OVERLORD).length === 2) {
         unitCommand.queueCommand = true;
-        unitCommand.targetWorldSpacePos = map.getNatural().townhallPosition;
+        unitCommand.targetWorldSpacePos = getMiddleOfStructure(map.getNatural().townhallPosition, townhall);
         collectedActions.push(unitCommand);
         await actions.sendAction(unitCommand);
         const thirds = map.getThirds();
-        unitCommand.targetWorldSpacePos = thirds[Math.floor(Math.random() * thirds.length)].townhallPosition;
+        unitCommand.targetWorldSpacePos = getMiddleOfStructure(thirds[Math.floor(Math.random() * thirds.length)].townhallPosition, townhall);
       } else {
         unitCommand.targetWorldSpacePos = getRandomPoint(map);
       }
