9efa314d9bb498f233f975af1b29cb10488f8325
Daniel-PC\Daniel <lostlucidity@gmail.com>
2022-10-15 19:36:53 -0400

    Premove queens for injection.

--- javascript/systems/injector-system.js
+++ javascript/systems/injector-system.js
@@ -2,11 +2,16 @@
 "use strict"
 
 const { createSystem } = require("@node-sc2/core");
-const { EFFECT_INJECTLARVA } = require("@node-sc2/core/constants/ability");
+const { EFFECT_INJECTLARVA, MOVE } = require("@node-sc2/core/constants/ability");
 const { QUEENSPAWNLARVATIMER } = require("@node-sc2/core/constants/buff");
 const { Race } = require("@node-sc2/core/constants/enums");
+const { QUEEN } = require("@node-sc2/core/constants/unit-type");
+const { areEqual } = require("@node-sc2/core/utils/geometry/point");
 const { createUnitCommand } = require("../services/actions-service");
-const { getClosestUnitByPath } = require("../services/resource-manager-service");
+const { getTimeInSeconds } = require("../services/frames-service");
+const { getClosestUnitByPath, getDistanceByPath, getClosestUnitPositionByPath } = require("../services/resource-manager-service");
+const { getMovementSpeed, getPendingOrders } = require("../services/unit-service");
+const { setPendingOrders } = require("./unit-resource/unit-resource-service");
 
 module.exports = createSystem({
   name: "InjectorSystem",
@@ -27,16 +32,93 @@ module.exports = createSystem({
  */
 function injectLarva(resources) {
   const { units } = resources.get();
-  const bases = units.getBases();
-  const injectUnits = units.getAlive().filter(unit => unit.canInject());
   const collectedActions = [];
-  injectUnits.forEach(injectUnit => {
-    const [closestBase] = getClosestUnitByPath(resources, injectUnit.pos, bases.filter(base => !base.buffIds.includes(QUEENSPAWNLARVATIMER)));
-    if (closestBase) {
-      const unitCommand = createUnitCommand(EFFECT_INJECTLARVA, [injectUnit]);
-      unitCommand.targetUnitTag = closestBase.tag;
-      collectedActions.push(unitCommand);
+  const baseAndQueenSpawnTimerLeft = getBaseAndQueenSpawnTimerLeft(resources);
+  units.getById(QUEEN).forEach(queen => {
+    const { energy, orders, pos } = queen;
+    if (energy === undefined || orders === undefined || pos === undefined) return;
+    const timeTo25Energy = (25 - energy) > 0 ? (25 - energy) / getEnergyRegenRate() : 0;
+    const [closestUnitByPath] = getClosestUnitByPath(resources, pos, baseAndQueenSpawnTimerLeft.filter(base => {
+      const { base: { pos: basePos }, timeLeft } = base;
+      if (basePos === undefined) return false;
+      const closestUnitPositionByPath = getClosestUnitPositionByPath(resources, basePos, pos);
+      const timeToDistance = getTimeToDistance(resources, queen, closestUnitPositionByPath);
+      return timeLeft <= timeTo25Energy || timeLeft <= timeToDistance;
+    }).map(base => base.base));
+    if (closestUnitByPath) {
+      const { buffIds, pos: closestUnitByPathPos } = closestUnitByPath;
+      if (buffIds === undefined || closestUnitByPathPos === undefined) return;
+      const ordersAndPendingOrders = getPendingOrders(queen).concat(orders);
+      if (queen.canInject()) {
+        const noInjectingOrder = ordersAndPendingOrders.every(order => order.abilityId !== EFFECT_INJECTLARVA);
+        if (noInjectingOrder && !buffIds.includes(QUEENSPAWNLARVATIMER)) {
+          const unitCommand = createUnitCommand(EFFECT_INJECTLARVA, [queen]);
+          unitCommand.targetUnitTag = closestUnitByPath.tag;
+          collectedActions.push(unitCommand);
+          setPendingOrders(queen, unitCommand);
+          closestUnitByPath.labels.delete('queenSpawnTimerLeft');
+        }
+      } else {
+        const [closestBase] = units.getClosest(closestUnitByPathPos, units.getBases());
+        const { pos: closestBasePos } = closestBase;
+        if (closestBasePos === undefined) return;
+        const noMoveOrderToBase = ordersAndPendingOrders.every(order => {
+          const { abilityId, targetWorldSpacePos } = order;
+          if (targetWorldSpacePos === undefined) return true; 
+          return abilityId !== MOVE || !areEqual(targetWorldSpacePos, closestBasePos);
+        });
+        if (noMoveOrderToBase) {
+          const unitCommand = createUnitCommand(MOVE, [queen]);
+          unitCommand.targetWorldSpacePos = closestBase.pos;
+          collectedActions.push(unitCommand);
+          setPendingOrders(queen, unitCommand);
+        }
+      }
     }
   });
   return collectedActions;
+}
+
+/**
+ * @param {ResourceManager} resources 
+ * @returns {{base: Unit, timeLeft: number}[]}
+ */
+function getBaseAndQueenSpawnTimerLeft(resources) {
+  const { frame, units } = resources.get();
+  return units.getBases().map(base => {
+    const { buffIds } = base;
+    if (buffIds === undefined) return { base, timeLeft: 0 };
+    if (buffIds.includes(QUEENSPAWNLARVATIMER)) {
+      if (!base.labels.get('queenSpawnLarvatimer')) {
+        base.labels.set('queenSpawnLarvatimer', frame.getGameLoop());
+        return { base, timeLeft: 40 / 1.4 };
+      } else {
+        const queenSpawnLarvatimer = base.labels.get('queenSpawnLarvatimer');
+        const timeLeft = 40 / 1.4 - getTimeInSeconds(frame.getGameLoop() - queenSpawnLarvatimer);
+        return { base, timeLeft };
+      }
+    } else {
+      base.labels.delete('queenSpawnLarvatimer');
+      return { base, timeLeft: 0 };
+    }
+  });
+}
+
+function getEnergyRegenRate() { 
+  return 0.7875;
+}
+
+/**
+ * @param {ResourceManager} resources 
+ * @param {Unit} unit 
+ * @param {Point2D} target 
+ * @returns {number}
+ */
+function getTimeToDistance(resources, unit, target) {
+  const { pos } = unit;
+  if (pos === undefined) return Infinity
+  const distanceByPath = getDistanceByPath(resources, pos, target);
+  const movementSpeed = getMovementSpeed(unit);
+  if (movementSpeed === undefined) return Infinity;
+  return distanceByPath / movementSpeed;
 }
