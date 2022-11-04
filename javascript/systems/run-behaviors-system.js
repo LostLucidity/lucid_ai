3d83d4cdfa4c753945b622d569076488e9f1430a
Daniel-PC\Daniel <lostlucidity@gmail.com>
2022-11-04 19:23:57 -0400

    Add barracks behavior for rallying to bogSystem.

--- javascript/systems/run-behaviors-system.js
+++ javascript/systems/run-behaviors-system.js
@@ -4,7 +4,7 @@
 const { createSystem } = require("@node-sc2/core");
 const { generalScouting } = require("../builds/scouting");
 const { scoutEnemyMainBehavior, clearFromEnemyBehavior } = require("../helper/behavior/labelled-behavior");
-const { supplyDepotBehavior, overlordBehavior, muleBehavior } = require("../helper/behavior/unit-behavior");
+const { supplyDepotBehavior, overlordBehavior, muleBehavior, barracksBehavior } = require("../helper/behavior/unit-behavior");
 const scoutService = require("./scouting/scouting-service");
 
 module.exports = createSystem({
@@ -20,6 +20,7 @@ module.exports = createSystem({
     const { resources } = world;
     const { actions } = resources.get();
     const collectedActions = [];
+    collectedActions.push(...barracksBehavior(resources));
     collectedActions.push(...clearFromEnemyBehavior(world));
     collectedActions.push(...overlordBehavior(world));
     collectedActions.push(...muleBehavior(resources));
