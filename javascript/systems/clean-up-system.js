245121e39fa296265762a49ff20db8180a084f0a
Daniel-PC\Daniel <lostlucidity@gmail.com>
2023-09-18 16:30:21 -0400

    Refactor and enhance CleanUpSystem.
    - Add JSDoc comments for better clarity and documentation.
    - Modularize the `onUpgradeCompleted` function for improved readability.
    - Fetch and update game data on upgrade completion.
    - Ensure caches and data structures are reset accordingly.
    - Use Set to track known upgrades and update it as required.

--- javascript/systems/clean-up-system.js
+++ javascript/systems/clean-up-system.js
@@ -9,13 +9,48 @@ const agentService = require("../services/agent-service");
 const worldService = require("../src/world-service");
 const MapResourceService = require("./map-resource-system/map-resource-service");
 
+let knownUpgrades = new Set();
 
+/**
+ * Handles tasks when an upgrade is completed.
+ * @param {World} world - The current world state.
+ * @param {number} upgradeId - The ID of the completed upgrade.
+ */
+async function onUpgradeCompleted(world, upgradeId) {
+  const { data, resources } = world;
+  console.log(`Upgrade ${upgradeId} completed`);
+
+  // Reset the movement speed cache.
+  unitService.movementSpeedByType = new Map();
+  const { actions: { _client } } = resources.get();
+  /** @type {SC2APIProtocol.ResponseData} */
+  let gameData;
+  if (_client) {
+    gameData = await _client.data({ unitTypeId: true });
+    ['units'].forEach((dataType) => {
+      if (gameData[dataType]) data.set(dataType, gameData[dataType]);
+    });
+  }
+
+  knownUpgrades.add(upgradeId);  // Update the known upgrades.
+}
+
+/**
+ * CleanUpSystem handles various tasks related to cleaning up data 
+ * structures and caches during the game's lifecycle.
+ */
 module.exports = createSystem({
   name: 'CleanUpSystem',
   type: 'agent',
+
+  /**
+   * Handle tasks on each step of the game.
+   */
   async onStep(world) {
-    const { resources } = world;
+    const { agent, resources } = world;
     const { units } = resources.get();
+
+    // Reset various caches and data structures.
     dataService.earmarks = [];
     MapResourceService.freeGasGeysersCache = new Map();
     sharedService.removePendingOrders(units);
@@ -24,12 +59,25 @@ module.exports = createSystem({
     agentService.hasTechFor = new Map();
     worldService.availableProductionUnits = new Map();
     worldService.productionUnitsCache = new Map();
+
+    const currentUpgrades = new Set(agent.upgradeIds);
+
+    for (const upgradeId of currentUpgrades) {
+      if (!knownUpgrades.has(upgradeId)) {
+        await onUpgradeCompleted(world, upgradeId);  // Await the asynchronous function.
+      }
+    }
   },
+
+  /**
+   * Handle tasks when a unit becomes idle.
+   */
   async onUnitIdle(world) {
     const { resources } = world;
     const { units } = resources.get();
+
     sharedService.removePendingOrders(units);
     unitService.selfDPSHealth = new Map();
     unitService.selfUnits = new Map();
-  }
+  },
 });
