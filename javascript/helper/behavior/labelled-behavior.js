99e3d2d62edfecf9a746a75dceed6e068813670e
Daniel-PC\Daniel <lostlucidity@gmail.com>
2023-09-26 10:35:36 -0400

    refactor: Optimize filtering for getPotentialCombatantsInRadius
    - Combined filtering operations to reduce iterations over units.
    - Added explicit checks for valid unit positions to address type issues.
    - Enhanced error handling for units without a defined position.
    
    This refactor aims to improve both performance and reliability by merging the radius and combatant checks, while also addressing potential type mismatches related to unit positions.

--- javascript/helper/behavior/labelled-behavior.js
+++ javascript/helper/behavior/labelled-behavior.js
@@ -602,8 +602,19 @@ function handleEngageOrRetreat(world, unit, enemyUnit, nearbyEnemyUnits, collect
  * @returns {Unit[]} - Array of potential combatant units.
  */
 function getPotentialCombatantsInRadius(world, unit, radius) {
-  const { units } = world.resources.get();
-  const unitsInRadius = unitService.getUnitsInRadius(units.getAlive(Alliance.SELF), radius);
-  return unitsInRadius.filter(unitService.potentialCombatants);
+  // Destructure to get the units directly
+  const units = world.resources.get().units;
+
+  // Use a single filtering operation to get potential combatants in the given radius.
+  return units.getAlive(Alliance.SELF).filter(targetUnit => {
+    // Check if both units have valid positions
+    if (!unit.pos || !targetUnit.pos) return false;
+
+    // Check if the target unit is within the radius
+    const isWithinRadius = getDistance(unit.pos, targetUnit.pos) <= radius;
+    // Check if the target unit is a potential combatant
+    const isPotentialCombatant = unitService.potentialCombatants(targetUnit);
+    return isWithinRadius && isPotentialCombatant;
+  });
 }
 
